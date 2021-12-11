1eaa2855d901bf6fb337053d008a7531e4098daf
Andy Chou <andychchou@gmail.com>
2021-12-11 00:18:58 -0800

    promotions added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const gameRouter = require('./routes/gameRouter');
 const postRouter = require('./routes/postRouter');
 const commentRouter = require('./routes/commentRouter');
 const technoRouter = require('./routes/technoRouter');
+const promotionRouter = require('./routes/promotionRouter');
 
 const mongoose = require('mongoose');
 
@@ -60,6 +61,7 @@ app.use('/games', gameRouter);
 app.use('/posts', postRouter);
 app.use('/comments', commentRouter);
 app.use('/technos', technoRouter);
+app.use('/promotions', promotionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
